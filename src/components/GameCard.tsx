9afc5ca77b88dd7c522b44a78ef0f4d2cf0ab718
Branson Rose <bransonrose+github@hotmail.com>
2026-09-03 15:22:04 -0400

    Created GameCard

---
+++ src/components/GameCard.tsx
@@ -0,0 +1,19 @@
+import { type Game } from "@/hooks/useGames";
+import { Card, CardBody, Image } from "@chakra-ui/react";
+
+interface GameCardProps {
+	game: Game;
+}
+
+function GameCard({ game }: GameCardProps) {
+	return (
+		<Card.Root borderRadius={10} overflow="hidden">
+			<Image src={game.background_image} />
+			<CardBody>
+				<Card.Title fontSize="2xl">{game.name}</Card.Title>
+			</CardBody>
+		</Card.Root>
+	);
+}
+
+export default GameCard;
