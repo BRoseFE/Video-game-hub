c3cf6b7c1fc30368f290c6a2d96134a21747f717
Branson Rose <bransonrose+github@hotmail.com>
2026-09-21 11:36:15 -0400

    Clean up gamecards

--- src/components/GameCard.tsx
+++ src/components/GameCard.tsx
@@ -13,13 +13,13 @@ function GameCard({ game }: GameCardProps) {
 		<Card.Root>
 			<Image src={getCroppedImageUrl(game.background_image)} />
 			<CardBody>
-				<Card.Title fontSize="2xl">{game.name}</Card.Title>
-				<HStack justifyContent="space-between">
+				<HStack justifyContent="space-between" marginBottom={3}>
 					<PlatformIconList
 						platforms={game.parent_platforms.map((p) => p.platform)}
 					/>
 					<MetaCritic criticScore={game.metacritic} />
 				</HStack>
+				<Card.Title fontSize="2xl">{game.name}</Card.Title>
 			</CardBody>
 		</Card.Root>
 	);
